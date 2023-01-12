ee95263034bdacfe49c97861054c4babd8bf1f59
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-01-12 16:36:35 +0100

    Hardhat init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-deploy";
+import "solidity-coverage";
+import "hardhat-contract-sizer";
+import "hardhat-gas-reporter";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
